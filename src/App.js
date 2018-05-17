3d6972fdde7af47cd36a2ef5a0a2b233c615ff96
miik <miik@localhost.localdomain>
2018-05-17 14:53:56 +0300

    beautify

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Grid from 'material-ui/Grid';
 import datajson from './data.json';
-import Table, { TableHead, TableBody, TableCell, TableRow } from 'material-ui/Table';
+import Table, {TableHead, TableBody, TableCell, TableRow} from 'material-ui/Table';
 import CustomTableRow from './CustomTableRow';
 
 class App extends Component {
 
-  constructor(props){
+  constructor(props) {
     super();
-    this.state = {persons:[]};
+    this.state = {
+      persons: []
+    };
   }
 
   componentDidMount = () => {
     //fetch('url.com/data.json')
-        //.then(response => response.json())
-        /*.then(datajson => */ this.setState({persons: datajson}) //)
+    //.then(response => response.json())
+    /* .then(datajson => */
+    this.setState({persons: datajson}) //)
     //.catch(err => console.error(err));
   }
 
@@ -25,44 +28,49 @@ class App extends Component {
 
   deletePerson = (delPerson) => {
     let newPersonList = this.state.persons.filter(filPerson => filPerson.id !== delPerson.id);
-    this.setState({persons:newPersonList});
+    this.setState({persons: newPersonList});
   }
 
   logPersons = () => console.log(this.state.persons);
 
   savePerson = (person) => {
-    this.setState({persons: this.state.persons.map(p=>p.id===person.id?Object.assign({},p,{name:person.name,email:person.email,phone:person.phone}):p)})
+    this.setState({
+      persons: this.state.persons.map(
+        p => p.id === person.id
+        ? Object.assign({}, p, {
+          name: person.name,
+          email: person.email,
+          phone: person.phone
+        })
+        : p)
+    })
   }
 
   render() {
-    return (
-      <div className="App">
-        <Grid container justify='center'>
-          <Grid item xs={10}>
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell>Name</TableCell>
-                  <TableCell>Email</TableCell>
-                  <TableCell>Phone</TableCell>
-                  <TableCell>Edit</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {
-                  this.state.persons.map(person => {
-                    return (
-                      <CustomTableRow person={person} deletePerson={this.deletePerson} savePerson={this.savePerson} key={person.id}></CustomTableRow>
-                    )
-                  })
-                }
-              </TableBody>
-            </Table>
-          </Grid>
+    return (<div className="App">
+      <Grid container="container" justify='center'>
+        <Grid item="item" xs={10}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Name</TableCell>
+                <TableCell>Email</TableCell>
+                <TableCell>Phone</TableCell>
+                <TableCell>Edit</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {
+                this.state.persons.map(person => {
+                  return (<CustomTableRow person={person} deletePerson={this.deletePerson} savePerson={this.savePerson} key={person.id}></CustomTableRow>)
+                })
+              }
+            </TableBody>
+          </Table>
         </Grid>
-        <button onClick={this.logPersons}>logPersons</button>
-      </div>
-    );
+      </Grid>
+      <button onClick={this.logPersons}>logPersons</button>
+    </div>);
   }
 }
 
--- src/CustomTableRow.js
+++ src/CustomTableRow.js
@@ -1,15 +1,21 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {TableCell, TableRow} from 'material-ui/Table';
 import Input from 'material-ui/Input';
 
 export default class CustomTableRow extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state = {person:this.props.person};
+    this.state = {
+      person: this.props.person
+    };
   }
 
   enableEdit = () => {
-    this.setState(prevState => ({person: {...prevState.person, edit: true},
+    this.setState(prevState => ({
+      person: {
+        ...prevState.person,
+        edit: true
+      },
       name: this.state.person.name,
       email: this.state.person.email,
       phone: this.state.person.phone
@@ -17,7 +23,8 @@ export default class CustomTableRow extends Component {
   }
 
   savePerson = () => {
-    let newPerson = {...this.state.person,
+    let newPerson = {
+      ...this.state.person,
       name: this.state.name,
       email: this.state.email,
       phone: this.state.phone,
@@ -28,41 +35,50 @@ export default class CustomTableRow extends Component {
   }
 
   cancelSave = () => {
-    this.setState(prevState => ({person:{...prevState.person, edit: false}}));
+    this.setState(prevState => ({
+      person: {
+        ...prevState.person,
+        edit: false
+      }
+    }));
   }
 
   handleInputChange = (event) => {
-    this.setState({[event.target.name]:event.target.value});
+    this.setState({
+      [event.target.name]: event.target.value
+    });
   }
 
   render() {
-    return (
-      <TableRow key={this.state.person.id}>
-        <TableCell>{this.state.person.edit ?
-          <Input type="text" name="name" onChange={event => this.handleInputChange(event)} value={this.state.name}/> :
-          this.state.person.name
+    return (<TableRow key={this.state.person.id}>
+      <TableCell>{
+          this.state.person.edit
+            ? <Input type="text" name="name" onChange={event => this.handleInputChange(event)} value={this.state.name}/>
+            : this.state.person.name
         }</TableCell>
-        <TableCell>{this.state.person.edit ?
-          <Input type="text" name="email" onChange={event => this.handleInputChange(event)} value={this.state.email}/> :
-          this.state.person.email
+      <TableCell>{
+          this.state.person.edit
+            ? <Input type="text" name="email" onChange={event => this.handleInputChange(event)} value={this.state.email}/>
+            : this.state.person.email
         }</TableCell>
-        <TableCell>{this.state.person.edit ?
-          <Input type="text" name="phone" onChange={event => this.handleInputChange(event)} value={this.state.phone}/> :
-          this.state.person.phone
+      <TableCell>{
+          this.state.person.edit
+            ? <Input type="text" name="phone" onChange={event => this.handleInputChange(event)} value={this.state.phone}/>
+            : this.state.person.phone
         }</TableCell>
-        <TableCell>
-          {this.state.person.edit ?
-          <div>
-            <button onClick={() => this.savePerson()}>save</button>
-            <button onClick={() => this.cancelSave()}>cancel</button>
-          </div> :
-          <div>
-            <button onClick={() => this.enableEdit()}>edit</button>
-            <button onClick={() => this.props.deletePerson(this.props.person)}>delete</button>
-          </div>
-          }
-        </TableCell>
-      </TableRow>
-    );
+      <TableCell>
+        {
+          this.state.person.edit
+            ? <div>
+                <button onClick={() => this.savePerson()}>save</button>
+                <button onClick={() => this.cancelSave()}>cancel</button>
+              </div>
+            : <div>
+                <button onClick={() => this.enableEdit()}>edit</button>
+                <button onClick={() => this.props.deletePerson(this.props.person)}>delete</button>
+              </div>
+        }
+      </TableCell>
+    </TableRow>);
   }
 }
